28177ed1d069f9f417792295486964b8b863c997
ChaeYeon Lee <44535488+LeeChaeY@users.noreply.github.com>
2024-03-29 18:03:00 +0900

    Update 다른 정답.java

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/다른 정답.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/다른 정답.java
@@ -1,5 +1,3 @@
-### 다른 정답
-
 import java.util.*;
 
 class Solution {
@@ -12,5 +10,23 @@ class Solution {
             map.put(ch,i);
         }
         return answer;
+    }
+}
+
+
+class Solution {
+    public int[] solution(String str) {
+        int[] result = new int[str.length()];
+
+        for(int i=0;i<str.length();i++){
+
+            String subStr = str.substring(0,i);
+            if(subStr.indexOf(str.charAt(i))==-1) {
+                result[i] = -1;
+            }else {
+                result[i] = i-subStr.lastIndexOf(str.charAt(i));
+            }
+        }
+        return result;
     }
 }
