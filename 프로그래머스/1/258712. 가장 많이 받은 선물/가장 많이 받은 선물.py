6534fece64b2764366824d716c2adfbd4b47669b
ChaeYeon Lee <44535488+LeeChaeY@users.noreply.github.com>
2024-10-23 19:06:27 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.07 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -0,0 +1,28 @@
+def solution(friends, gifts):
+    d = {key:i for i, key in enumerate(friends)}
+    global sizes
+    size = len(friends)
+    answer = [0 for _ in range(size)]
+    gift_arr = [[0 for _ in range(size)] for _ in range(size)]
+    gift_num = [0 for _ in range(size)]
+    
+    for s in gifts:
+        p1, p2 = s.split()
+        gift_arr[d[p1]][d[p2]] += 1
+        gift_num[d[p1]] += 1
+        gift_num[d[p2]] -= 1
+    
+    print(gift_arr)
+    for i in range(size-1):
+        for j in range(i+1, size):
+            if ((gift_arr[i][j] == 0 and gift_arr[j][i] == 0) or (gift_arr[i][j] == gift_arr[j][i])):
+                if gift_num[i] > gift_num[j]:
+                    answer[i] += 1
+                elif gift_num[i] < gift_num[j]:
+                    answer[j] += 1
+            else:
+                if gift_arr[i][j] > gift_arr[j][i]:
+                    answer[i] += 1
+                elif gift_arr[i][j] < gift_arr[j][i]:
+                    answer[j] += 1
+    return max(answer)
