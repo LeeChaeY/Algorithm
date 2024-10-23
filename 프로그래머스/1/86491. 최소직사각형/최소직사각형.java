d4e3f5782ffa005f134b40f3c0d73283d02e95e1
ChaeYeon Lee <44535488+LeeChaeY@users.noreply.github.com>
2024-10-23 16:54:23 +0900

    Update 최소직사각형.java

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,3 +1,5 @@
+// 문제풀이 시간: 1st - 1시간 반 소요 후 실패, 2nd - 질문하기 힌트(https://school.programmers.co.kr/questions/20800 )보고 20분 소요 후 성공
+
 import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
