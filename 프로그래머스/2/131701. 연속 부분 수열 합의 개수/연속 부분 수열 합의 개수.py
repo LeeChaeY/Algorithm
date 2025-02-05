5d7773a8075a83c434a7369c77a8947223c09208
ChaeYeon Lee <44535488+LeeChaeY@users.noreply.github.com>
2025-02-05 19:16:38 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4267.41 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,10 @@
+# 7:00
+def solution(elements):
+    # ans = elements+ [sum(elements)]
+    ans = set(elements+[sum(elements)])
+    size = len(elements)
+    elements *= 2
+    for i in range(2, size):
+        for j in range(size):
+            ans.add(sum(elements[j:j+i]))
+    return len(ans)
